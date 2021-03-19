856a0fdb3fcc2876121bbc3793e3f1c77dc3e152
silvasistemas <silvasistemas@gmail.com>
2021-03-19 14:34:01 -0300

    Classes de dominio inicializadas

---
+++ src/main/java/com/silvasistemas/bookstore/domain/Livro.java
@@ -0,0 +1,11 @@
+package com.silvasistemas.bookstore.domain;
+
+public class Livro {
+	
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+	
+	private Categoria categoria;
+}
